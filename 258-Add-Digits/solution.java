e14039db962d25273e403d7c7e21c472839480f1
abhinavc4 <abhinavc@buffalo.edu>
2016-08-04 09:47:03 -0700

    [258. Add Digits][Accepted]committed by LeetCode Extension

---
+++ 258-Add-Digits/solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int addDigits(int num) {
+        if(num<1)
+        {
+            return num;
+        }
+        int mod = num%9;
+        return mod==0?9:mod;
+    }
+}
