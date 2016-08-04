fc7966e842cc6b8ef9966dc02a38d00f489f18eb
abhinavc4 <abhinavc@buffalo.edu>
2016-08-04 13:17:55 -0700

    [171. Excel Sheet Column Number][Accepted]committed by LeetCode Extension

--- 171-Excel-Sheet-Column-Number/solution.java
+++ 171-Excel-Sheet-Column-Number/solution.java
@@ -1,11 +1,11 @@
 public class Solution {
     public int titleToNumber(String s) {
         int len = s.length();
-        s = s.toLowerCase();
+        //s = s.toLowerCase();
         int ret = 0;
         for(int i = 0 ; i < len ;i++)
         {
-            ret= ret*26 + s.charAt(i)-'a' + 1;
+            ret= ret*26 + s.charAt(i)-'A' + 1;
         }
         return ret;
     }
