fedc2d1ddf7d31809e16ce81c13bfa3da81e2d5b
abhinavc4 <abhinavc@buffalo.edu>
2016-08-04 14:07:49 -0700

    [169. Majority Element][Accepted]committed by LeetCode Extension

---
+++ 169-Majority-Element/solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public int majorityElement(int[] nums) {
+        if(nums.length==0)
+        {
+            return 0;
+        }
+        int elem = nums[0];
+        int count = 1;
+        for(int i = 1 ; i < nums.length;i++)
+        {
+            if(nums[i]==elem)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+                if(count == 0 )
+                {
+                    elem = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return elem;
+    }
+}
