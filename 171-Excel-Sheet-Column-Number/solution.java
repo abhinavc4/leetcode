b5036687170714800e7670643634c881bd7a705c
abhinavc4 <abhinavc@buffalo.edu>
2016-08-04 13:21:08 -0700

    Update solution.java

--- 171-Excel-Sheet-Column-Number/solution.java
+++ 171-Excel-Sheet-Column-Number/solution.java
@@ -1,7 +1,6 @@
 public class Solution {
     public int titleToNumber(String s) {
         int len = s.length();
-        //s = s.toLowerCase();
         int ret = 0;
         char[] chArr = s.toCharArray();
         for(int i = 0 ; i < len ;i++)
