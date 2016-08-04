651fc951b8fcdf6611411a133fcd1be1ffd1a2e1
abhinavc4 <abhinavc@buffalo.edu>
2016-08-04 01:14:30 -0700

    [371. Sum of Two Integers][Time Limit Exceeded]committed by LeetCode Extension

--- 371-Sum-of-Two-Integers/solution.java
+++ 371-Sum-of-Two-Integers/solution.java
@@ -1,12 +1,21 @@
+
+
 public class Solution {
     public int getSum(int a, int b) {
- 
-   while(b!=0){
-       int c = a&b;
-       a=a^b;
-       b=c<<1;
-   }
- 
-   return a;
-}
+        int sum = 0 ;
+        int carry = 0;
+        int count = 0;
+        while(a!=0||b!=0)
+        {
+            int a1 = (a&1);
+            int b1 = (b&1);
+            a = a>>1;
+            b = b>>1;
+            int tempSum = a1^b1^carry;
+            carry = (a1&b1)|(a1&carry)|(b1&carry);
+            tempSum = tempSum<<count++;
+            sum = sum|tempSum;
+        }
+        return (sum|carry<<count);
+    }
 }
