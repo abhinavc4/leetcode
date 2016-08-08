7b85619aa12f6a31f951f1acf1d39ab039384ef5
abhinavc4 <abhinavc@buffalo.edu>
2016-08-08 15:26:05 -0700

    [101. Symmetric Tree][Accepted]committed by LeetCode Extension

--- 101-Symmetric-Tree/solution.java
+++ 101-Symmetric-Tree/solution.java
@@ -9,60 +9,21 @@
  */
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root == null)
+        return isSym(root,root);
+    }
+    
+    public boolean isSym(TreeNode left , TreeNode right)
+    {
+        if(left == null && right == null)
         {
             return true;
         }
-        Queue <TreeNode> q = new LinkedList<TreeNode>();
-        q.add(root);
-        ArrayList<Integer> al = new ArrayList<Integer>();
-        int size = q.size();
-        while(!q.isEmpty())
+        if(left == null || right == null)
         {
-            TreeNode curr = q.remove();
-            if(curr!=null)
-            {
-                q.add(curr.left);
-                q.add(curr.right);
-                al.add(curr.val);
-            }
-            else
-            {
-                al.add(null);
-            }
-            size--;
-            if(size == 0)
-            {
-                size = q.size();
-                if(isSym(al))
-                {
-                    al.clear();
-                }
-                else
-                {
-                    return false;
-                }
-            }
+            return false;
         }
-        return true;
-    }
-    
-    boolean isSym(ArrayList<Integer> al )
-    {
-        int i = 0 ; int j = al.size()-1;
-        while(i<j)
-        {
-            if(al.get(i) == al.get(j))
-            {
-                i++;
-                j--;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        
-        return true;
+        return (left.val == right.val) &&
+        isSym(left.left,right.right) &&
+        isSym(left.right,right.left);
     }
 }
