512b15ad7622ce9af1714bfdf6739a32f1528968
abhinavc4 <abhinavc@buffalo.edu>
2016-08-08 20:29:04 -0700

    [232. Implement Queue using Stacks][Accepted]committed by LeetCode Extension

---
+++ 232-Implement-Queue-using-Stacks/solution.java
@@ -0,0 +1,50 @@
+class MyQueue {
+    // Push element x to the back of queue.
+    Deque<Integer> stackOne = new LinkedList<Integer>();
+    Deque<Integer> stackTwo = new LinkedList<Integer>();
+    public void push(int x) {
+        
+        if(stackTwo.size()==0)
+        {
+            stackOne.addFirst(x);
+        }
+        else
+        {
+            stackOne.addFirst(x);
+        }
+    }
+
+    // Removes the element from in front of queue.
+    public void pop() {
+        if(stackTwo.size()!=0)
+        {
+            stackTwo.removeFirst();
+            return;
+        }
+        
+        while(stackOne.size()!=0)
+        {
+            stackTwo.addFirst(stackOne.removeFirst());    
+        }
+        stackTwo.removeFirst();
+    }
+
+    // Get the front element.
+    public int peek() {
+        if(stackTwo.size()!=0)
+        {
+            return stackTwo.peekFirst();
+        }
+        
+        while(stackOne.size()!=0)
+        {
+            stackTwo.addFirst(stackOne.removeFirst());    
+        }
+        return stackTwo.peekFirst();
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+        return stackOne.isEmpty()&&stackTwo.isEmpty();
+    }
+}
