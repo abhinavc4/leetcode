1e29fc7ccce77599310c42a7b4459a7565965b3e
abhinavc4 <abhinavc@buffalo.edu>
2016-08-11 17:36:10 -0700

    [168. Excel Sheet Column Title][Accepted]committed by LeetCode Extension

--- 168-Excel-Sheet-Column-Title/solution.java
+++ 168-Excel-Sheet-Column-Title/solution.java
@@ -5,27 +5,12 @@ public class Solution {
         {
             return s.toString();
         }
-        int c = 0;
+        
         while(n>0)
         {
-            int temp = n/(int)Math.pow(26,c);
-            int ch = temp%26;
-            if(ch ==0)
-            {
-                ch = 26;
-            }
-            s.insert(0,(char)('A'+ch-1));
-            int sub = 0;
-            if(c == 0)
-            {
-                sub = ch;
-            }
-            else
-            {
-                sub = (int)Math.pow(26,c)*ch;
-            }
-            c++;
-            n = n-sub;
+            n--;
+            s.insert(0,(char)('A'+(n%26)));
+            n=n/26;
         }
         return s.toString();
         
