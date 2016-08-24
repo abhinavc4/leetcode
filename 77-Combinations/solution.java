58df0b31fb8be1933f50023699b2871f97f3cc32
abhinavc4 <abhinavc@buffalo.edu>
2016-08-24 17:13:06 -0400

    [77. Combinations][Accepted]committed by LeetCode Extension

---
+++ 77-Combinations/solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> l = new ArrayList<List<Integer>>();
+        if( n<=0 || k<=0)
+        {
+            return l;
+        }
+        helper(n,k,1,new ArrayList<Integer>(),l);
+        return l;
+    }
+    void helper(int n , int k , int i , ArrayList<Integer> al ,List<List<Integer>> l )
+    {
+        if(al.size()==k)
+        {
+            l.add(new ArrayList<Integer>(al));
+            return;
+        }
+        for(int j = i ; j <=n;j++)
+        {
+            if(al.contains(j))
+            {
+                continue;
+            }
+            al.add(j);
+            helper(n,k,j+1,al,l);
+            al.remove(al.size()-1);
+        }
+    }
+}
