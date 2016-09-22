5284da55980a0131501a56d943944792c7bda439
abhinavc4 <abhinavc@buffalo.edu>
2016-09-22 15:13:01 -0400

    [137. Single Number II][Accepted]committed by LeetCode Extension

--- 137-Single-Number-II/solution.java
+++ 137-Single-Number-II/solution.java
@@ -1,16 +1,10 @@
 public class Solution {
     public int singleNumber(int[] nums) {
-        int a = 0 ;
-        int b = 0;
-        int bitmask = 0;
-        for(int i : nums)
-        {
-            b |= i&a ;
-            a = a^i;
-            bitmask = ~(a&b);
-            a = a&bitmask;
-            b = b&bitmask;
+        int ones = 0, twos = 0;
+        for(int i = 0; i < nums.length; i++){
+        ones = (ones ^ nums[i]) & ~twos;
+        twos = (twos ^ nums[i]) & ~ones;
         }
-        return a;
+        return ones;
     }
 }
