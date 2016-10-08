03f4833db168f51a429ecd0dbc10793972a683d7
abhinavc4 <abhinavc@buffalo.edu>
2016-10-08 06:19:32 -0400

    [166. Fraction to Recurring Decimal][Accepted]committed by LeetCode Extension

---
+++ 166-Fraction-to-Recurring-Decimal/solution.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+        if(numerator==0)
+        {
+            return "0";
+        }
+        StringBuilder str = new StringBuilder();
+        if(numerator<0 ^ denominator<0)
+        {
+            str.append("-");
+        }
+        Long divisor = Math.abs(Long.valueOf(numerator));
+        Long dividend = Math.abs(Long.valueOf(denominator));
+        str.append(String.valueOf(divisor/dividend));
+        Long remainder = divisor % dividend;
+        if(remainder == 0)
+        {
+            return str.toString();
+        }
+        str.append(".");
+        HashMap<Long,Integer> hm = new HashMap<>();
+        while(remainder!=0)
+        {
+            if(hm.containsKey(remainder))
+            {
+                str.insert(hm.get(remainder),"(");
+                str.append(")");
+                break;
+            }
+            hm.put(remainder,str.length());
+            remainder*=10;
+            str.append(String.valueOf(remainder/dividend));
+            remainder %= dividend;
+        }
+        return str.toString();
+    }
+}
