0c4dc2f5ff6b5fed2fa8f4a68d698097bf053666
abhinavc4 <abhinavc@buffalo.edu>
2016-10-11 15:06:11 -0400

    [389. Find the Difference][Accepted]committed by LeetCode Extension

--- 389-Find-the-Difference/solution.java
+++ 389-Find-the-Difference/solution.java
@@ -1,11 +1,25 @@
 public class Solution {
     public char findTheDifference(String s, String t) {
         char val = 0;
-        for(int i = 0  ;i < t.length()-1 ; i++)
+        /*for(int i = 0  ;i < t.length()-1 ; i++)
         {
             val = (char)(val^(s.charAt(i))^(t.charAt(i)));
         }
         val = (char)(val^t.charAt(t.length()-1));
+        return val;*/
+        int i = 0 ;
+        int j = 0 ;
+        while(i<s.length() || j<t.length())
+        {
+            if(i<s.length())
+            {
+                val = (char)(val^s.charAt(i++));
+            }
+            if(j<t.length())
+            {
+                val = (char)(val^t.charAt(j++));
+            }
+        }
         return val;
     }
 }
